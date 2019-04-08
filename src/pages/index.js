3b304230a70ac30085cd20d1457a831158800e8f
FormBucket Support <support@formbucket.com>
2019-04-07 21:45:33 -0700

    Add twitter link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import styled from 'styled-components'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import styled from "styled-components";
 
-import { Layout, Article, Wrapper, Button, SectionTitle } from '../components'
+import { Layout, Article, Wrapper, Button, SectionTitle } from "../components";
 
 const Content = styled.div`
   grid-column: 2;
@@ -11,7 +11,7 @@ const Content = styled.div`
   border-radius: 1rem;
   padding: 3rem 6rem;
   background-color: white;
-  
+
   @media (max-width: ${props => props.theme.breakpoints.tablet}) {
     padding: 3rem 2rem;
   }
@@ -19,7 +19,7 @@ const Content = styled.div`
     padding: 2rem 1.5rem;
   }
   overflow: hidden;
-`
+`;
 
 const Hero = styled.div`
   grid-column: 2;
@@ -41,28 +41,51 @@ const Hero = styled.div`
       font-size: 1.25rem;
     }
   }
-`
+`;
 
 const IndexPage = ({
   data: {
-    allMdx: { edges: postEdges },
-  },
+    allMdx: { edges: postEdges }
+  }
 }) => (
   <Layout>
     <Wrapper>
       <Hero>
         <h1>Hi.</h1>
         <p>
-          I&apos;m <strong>Still Surviving</strong> with a crazy ex, not my own, it came with the package when I met my husband and his young daughter. If I only knew then what 10 years later would be like. My story is real but the names are changed to protect those I love.
+          I&apos;m <strong>Still Surviving</strong> with a crazy ex, not my own,
+          it came with the package when I met my husband and his young daughter.
+          If I only knew then what 10 years later would be like. My story is
+          real but the names are changed to protect those I love.
         </p>
-        <Link to="/contact">
-          <Button big>
-            <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
-            </svg>
-            Contact
-          </Button>
-        </Link>
+        <div>
+          <Link to="/contact">
+            <Button big>
+              <svg
+                width="1792"
+                height="1792"
+                viewBox="0 0 1792 1792"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
+              </svg>
+              Contact
+            </Button>
+          </Link>{" "}
+          <Link to="https://twitter.com/LifeWithACrazy1">
+            <Button big>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+                <g id="Logo_FIXED" data-name="Logo — FIXED">
+                  <path
+                    className="cls-2"
+                    d="M153.62,301.59c94.34,0,145.94-78.16,145.94-145.94,0-2.22,0-4.43-.15-6.63A104.36,104.36,0,0,0,325,122.47a102.38,102.38,0,0,1-29.46,8.07,51.47,51.47,0,0,0,22.55-28.37,102.79,102.79,0,0,1-32.57,12.45,51.34,51.34,0,0,0-87.41,46.78A145.62,145.62,0,0,1,92.4,107.81a51.33,51.33,0,0,0,15.88,68.47A50.91,50.91,0,0,1,85,169.86c0,.21,0,.43,0,.65a51.31,51.31,0,0,0,41.15,50.28,51.21,51.21,0,0,1-23.16.88,51.35,51.35,0,0,0,47.92,35.62,102.92,102.92,0,0,1-63.7,22A104.41,104.41,0,0,1,75,278.55a145.21,145.21,0,0,0,78.62,23"
+                  />
+                </g>
+              </svg>
+              Twitter
+            </Button>
+          </Link>
+        </div>
       </Hero>
       <Content>
         <SectionTitle>Latest stories</SectionTitle>
@@ -80,17 +103,17 @@ const IndexPage = ({
       </Content>
     </Wrapper>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     allMdx: PropTypes.shape({
-      edges: PropTypes.array.isRequired,
-    }),
-  }).isRequired,
-}
+      edges: PropTypes.array.isRequired
+    })
+  }).isRequired
+};
 
 export const IndexQuery = graphql`
   query IndexQuery {
@@ -111,4 +134,4 @@ export const IndexQuery = graphql`
       }
     }
   }
-`
+`;
